d5d314ed3305d41f6d3e7ff1d3ab1a548fe95cb9
OnlyFrogs <bonaventuratommasosam@gmail.com>
2026-07-08 16:36:37 +0200

    Make BuildingComponent spawn a visible cube by default for quick testing

--- Source/GribbitTown/GribbitBuildingComponent.cpp
+++ Source/GribbitTown/GribbitBuildingComponent.cpp
@@ -1,9 +1,16 @@
 #include "GribbitBuildingComponent.h"
 #include "GameFramework/Character.h"
 #include "Engine/World.h"
+#include "Engine/StaticMeshActor.h"
+#include "Components/StaticMeshComponent.h"
 
 UGribbitBuildingComponent::UGribbitBuildingComponent()
 {
+	// Default to a simple cube if nothing is assigned
+	if (!DefaultPlaceableObject)
+	{
+		DefaultPlaceableObject = AStaticMeshActor::StaticClass();
+	}
 }
 
 bool UGribbitBuildingComponent::PlaceObjectInFront(TSubclassOf<AActor> ObjectClass, float Distance)
@@ -24,5 +31,16 @@ bool UGribbitBuildingComponent::PlaceObjectInFront(TSubclassOf<AActor> ObjectCla
 	FRotator FinalRotation(0.f, Rotation.Yaw, 0.f);
 
 	AActor* NewActor = World->SpawnActor<AActor>(ObjectClass, Location, FinalRotation);
+
+	// If it's a StaticMeshActor, give it a visible cube
+	if (AStaticMeshActor* MeshActor = Cast<AStaticMeshActor>(NewActor))
+	{
+		UStaticMesh* CubeMesh = LoadObject<UStaticMesh>(nullptr, TEXT("/Engine/BasicShapes/Cube"));
+		if (CubeMesh)
+		{
+			MeshActor->GetStaticMeshComponent()->SetStaticMesh(CubeMesh);
+		}
+	}
+
 	return NewActor != nullptr;
 }
